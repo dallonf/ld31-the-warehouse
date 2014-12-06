92f48d94726cf74225671986a88faa5b8b373aa6
Dallon Feldner <dallonfdev@gmail.com>
2014-12-06 11:54:50 -0600

    Delivery zone is now dynamic

---
+++ Assets/Scripts/DeliveryZoneText.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DeliveryZoneText : MonoBehaviour
+{
+    private Text text;
+
+    public void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void Start()
+    {
+        UpdateText();
+    }
+
+    public void FixedUpdate()
+    {
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        text.text = string.Format("BOXES\n{0}/{1}", GameController.Instance.BoxesDelivered, GameController.Instance.BoxSequence.Length);
+    }
+}
