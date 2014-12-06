5ae64bdd8808fcc68d8e195dff9d5fe39a14812c
Dallon Feldner <dallonfdev@gmail.com>
2014-12-06 10:52:09 -0600

    Box goes away when you pick it up

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxController : MonoBehaviour
+{
+    public bool IsTargetBox = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        var player = coll.gameObject.GetComponent<PlayerController>();
+        if (player && !player.IsCarryingBox)
+        {
+            player.IsCarryingBox = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
